228de228f1e40b5a7dbdba2e9927ebd1afb6885a
Tomoya Nojima <tonojima0307@gmail.com>
2018-05-22 22:32:06 +0900

    Made Second Stage

--- Assets/Script/gameManager2.cs
+++ Assets/Script/gameManager2.cs
@@ -3,37 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class gameManager : MonoBehaviour
+public class gameManager2 : MonoBehaviour
 {
-
-
 	int random;
 	//	タイマーを数え表示させるためのもの
 	float timer;
 	public Text timerText;
+	int[] toClickButtonNumber;
 	//	何回までボタンを押せるのか
+	int clickButtonCount=0;
 	int whiteCount = 2;
 	int blueCount = 3;
 	int redCount = 4;
-	//	押さないといけないボタンの番号
-	int clickButtonCount = 1;
 	string selectColor;
 	bool clickOk = false;
-	// Use this for initialization
 	int counter = 0;
 	AudioSource audioSource;
 	public AudioClip goodSound;
 	public AudioClip badSound;
 
-	void Start ()
-	{
+
+	void Start(){
 		int[] number = new int[9];
-		int[] clickNumber = new int[9];
-//		int型の配列に１〜９の番号を格納
+		toClickButtonNumber = new int[9];
+//		ランダムに数字を配列に格納
 		for (int i = 0; i < number.Length; i++) {
-			number [i] = i + 1;
+			number [i] = Random.Range (1, 100);
+//			Debug.Log (number [i]);
 		}
-//		i番目とランダムの箱の内容を入れ替える
+
+//		バブルソート
+		// 最後の要素を除いて、すべての要素を並べ替える
+		for(int i=0;i<number.Length-1;i++){
+
+			// 下から上に順番に比較します
+			for(int j=number.Length-1;j>i;j--){
+
+				// 上の方が大きいときは互いに入れ替える
+				if(number[j]<number[j-1]){
+					int t=number[j];
+					number[j]=number[j-1];
+					number[j-1]=t;
+				}
+			}
+		}
+			
+		for (int i = 0; i < number.Length; i++) {
+//			押すべきボタンに、昇順の値が格納される
+			toClickButtonNumber [i] = number [i];
+		}
+//		中身を入れ替える
 		for (int i = 0; i < number.Length; i++) {
 			random = Random.Range (0, 9);
 			int temp = number [i];
@@ -41,7 +60,7 @@ public class gameManager : MonoBehaviour
 			number [random] = temp;
 
 		}
-//		色と番号を格納していく
+		//		色と番号を格納していく
 		for (int i = 0; i < number.Length; i++) {
 			int randomColor = Random.Range (1, 4);
 			if (randomColor > 2) {
@@ -64,11 +83,11 @@ public class gameManager : MonoBehaviour
 		audioSource = this.gameObject.GetComponent<AudioSource> ();
 
 	}
-	
 	// Update is called once per frames
 	void Update ()
 	{
 		Timer ();
+
 	}
 	//	タイマー
 	void Timer ()
@@ -79,7 +98,7 @@ public class gameManager : MonoBehaviour
 
 	public void CountButton (int k)
 	{
-		if (k == clickButtonCount) {
+		if (k == toClickButtonNumber[clickButtonCount]) {
 			Debug.Log (k);
 			audioSource.clip = goodSound;
 			audioSource.Play ();
@@ -92,12 +111,12 @@ public class gameManager : MonoBehaviour
 
 	public void ColorButton (string s)
 	{
-//		空じゃなかったら色を出力
+		//		空じゃなかったら色を出力
 		if (clickOk == true) {
 			counter++;
-//				押したボタンが何色かどうか
+			//				押したボタンが何色かどうか
 			if (s == "red") {
-//					それぞれのボタンの押すべき回数を超えたか
+				//					それぞれのボタンの押すべき回数を超えたか
 				if (counter >= redCount) {
 					counter = 0;
 					clickButtonCount++;
@@ -117,13 +136,15 @@ public class gameManager : MonoBehaviour
 			}
 
 			clickOk = false;
-			if (clickButtonCount > 9) {
+			if (clickButtonCount > 8) {
 				Debug.Log ("Finish!! Your Time is" + timer);
 			}
 		}
 	}
 
 
+
+
+
+
 }
-
-
